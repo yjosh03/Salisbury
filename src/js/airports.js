4ad01817ea69352aed5909c06a079089f4f72361
yjosh03 <33265167+yjosh03@users.noreply.github.com>
2017-12-10 17:52:31 -0500

    Update and rename airports.json to airports.js

---
+++ src/js/airports.js
@@ -0,0 +1,15 @@
+var airport =
+{
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Type":"Airport", "Name": "Spring Hill" }, "geometry": { "type": "Point", "coordinates": [ -75.651312717906038, 38.434823682526577 ] } },
+{ "type": "Feature", "properties": { "Type":"Airport", "Name": "Barren Creek" }, "geometry": { "type": "Point", "coordinates": [ -75.797162902669726, 38.455367849837067 ] } },
+{ "type": "Feature", "properties": { "Type":"Airport", "Name": "DelMarVa Air" }, "geometry": { "type": "Point", "coordinates": [ -75.402954817569054, 38.442073383983548 ] } },
+{ "type": "Feature", "properties": { "Type":"Airport", "Name": "Salisbury -Ocean City Regional" }, "geometry": { "type": "Point", "coordinates": [ -75.509476038745149, 38.340173794850223 ] } },
+{ "type": "Feature", "properties": { "Type":"Airport", "Name": "Bennett" }, "geometry": { "type": "Point", "coordinates": [ -75.616927131224642, 38.412753855650514 ] } },
+{ "type": "Feature", "properties": { "Type":"Airport", "Name": "Peninsula Regional Medical Center" }, "geometry": { "type": "Point", "coordinates": [ -75.583329714695452, 38.349984544820799 ] } },
+{ "type": "Feature", "properties": { "Type":"Airport", "Name": "Ennis Aerodome" }, "geometry": { "type": "Point", "coordinates": [ -75.557419726431519, 38.394545914484809 ] } },
+{ "type": "Feature", "properties": { "Type":"Airport", "Name": "Tim's Airport" }, "geometry": { "type": "Point", "coordinates": [ -75.62777412299036, 38.433317847921863 ] } }
+]
+}
